cffb5c90384afc1cc44dd3d8431b3b19e7abf396
Mathew Warger <mwarger@gmail.com>
2017-07-27 09:10:09 -0500

    added state

--- src/App.js
+++ src/App.js
@@ -1,20 +1,28 @@
 import React, { Component } from 'react'
 import logo from './logo.svg'
 import './App.css'
+import { state } from './state'
 
 const immutableES6StyleStateUpdate = person => (state, props) => {
   const { people } = state
   const personToUpdate = people.filter(p => p.id === person.id)[0]
   personToUpdate.name = personToUpdate.name === 'Jerry' ? 'Tom' : 'Jerry'
 
+  people[people.indexOf(person)].name = 'Banana'
+
   const newState = {
-    ...state, // spread all the old state, the people that come next will overwrite the old array of objects
-    people: [
-      ...people.slice(0, people.indexOf(person)), // take everything up to (but not including) the target person
-      personToUpdate, // shove in the update in place
-      ...people.slice(people.indexOf(person) + 1), // spread (...) everything after
-    ],
+    ...state,
+    people
   }
+
+  // const newState = {
+  //   ...state, // spread all the old state, the people that come next will overwrite the old array of objects
+  //   people: [
+  //     ...people.slice(0, people.indexOf(person)), // take everything up to (but not including) the target person
+  //     personToUpdate, // shove in the update in place
+  //     ...people.slice(people.indexOf(person) + 1), // spread (...) everything after
+  //   ],
+  // }
 
   return newState
 }
@@ -37,19 +45,7 @@ const People = (
 class App extends Component {
   constructor(props) {
     super(props)
-    this.state = {
-      fruit: 'banana',
-      people: [
-        {
-          name: 'Tom',
-          id: 234,
-        },
-        {
-          name: 'Drew',
-          id: 123,
-        },
-      ],
-    }
+    this.state = state
   }
 
   toggleTomAndJerry = person =>
@@ -62,13 +58,9 @@ class App extends Component {
           <img src={logo} className="App-logo" alt="logo" />
           <h2>Welcome to Drew's Example</h2>
         </div>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-        <People
-          peopleList={this.state.people}
-          toggleTomAndJerry={this.toggleTomAndJerry}
-        />
+        <pre>
+          {JSON.stringify(this.state, null, 2)}
+        </pre>
       </div>
     )
   }
